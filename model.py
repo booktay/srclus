e21fdb3fd57a0cfe5eef75d17eddd6486ff61664
Siwanont Sittinam <siwanont1@gmail.com>
2018-10-31 00:44:41 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
+
 class Template(object):
+    
     def __init__(self):
         self.value = "hello"
         self.other_value = "bonjour"
         self.constant_value = 42
 
-        for cls in reversed(self.__class__.mro()):
-            if hasattr(cls, 'init'):
-                cls.init(self)
+    
