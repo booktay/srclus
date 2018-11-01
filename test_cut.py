e080f26f112e304e966795490b9a5064319ddc8d
Siwanont Sittinam <siwanont1@gmail.com>
2018-11-01 11:29:36 +0700

    Update test_cut.py

--- test_cut.py
+++ test_cut.py
@@ -3,7 +3,7 @@ srcm = srcmodel()
 
 def main():
     # srcm.poolCreateModel(START=1*10**5, STOP=2*10**5, ITERATION=2*10**4)
-    srcm.poolCreateModel(START=1*10**5, STOP=1*10**5 + 500, ITERATION=100)
+    srcm.poolCreateModel(START=1*10**5, STOP=1*10**5 + 1000, ITERATION=200)
     # srcm.poolCreateModel(START=1*10**5, STOP=1*10**5 + 10, ITERATION=2)
 
 if __name__ == '__main__':
