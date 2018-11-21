1b13bae39e88031aebd2e4a46ea9d608c9ee4829
Siwanont Sittinam <siwanont1@gmail.com>
2018-11-21 13:59:43 +0700

    Update 2.test_model.py

--- 2.test_model.py
+++ 2.test_model.py
@@ -4,12 +4,13 @@ import os
 import json
 from word2vec import word2vec
 from srcmodel import srcmodel
+
 def openfile():
     DATA_ALL = []
     # PATH_CHECK = ["token.980000.1000000.201811101445.json"]
-    PATH_CHECK = os.listdir("data")
+    PATH_CHECK = os.listdir("data/token/")
     for one in PATH_CHECK:
-        with open(os.path.join("data/", one), 'r', encoding="utf-8") as data:
+        with open(os.path.join("data/token/", one), 'r', encoding="utf-8") as data:
             DATA_ALL = DATA_ALL + json.load(data)
     return DATA_ALL
 
