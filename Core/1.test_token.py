bfecb4b14bf036bcf7fc1d087cb5272650cf2fbf
Siwanont Sittinam <siwanont1@gmail.com>
2018-11-26 05:39:36 +0700

    Update 1.test_token.py

--- Core/1.test_token.py
+++ Core/1.test_token.py
@@ -1,5 +1,5 @@
 from srcluslib.predata import srclusdata
 srclusdata = srclusdata()
-srclusdata.createPoolGetFromUrl(2)
+srclusdata.createPoolGetFromUrl(4)
 # srclusdata.runGetFromUrl([0, 100])
 # print(srclusdata.getWordFromUrl(30000107))
