63b16866b56ef3f6ffaf2cf3bebbc1ef2c755594
Siwanont Sittinam <siwanont1@gmail.com>
2018-11-26 18:43:51 +0700

    Clean All

--- Core/1.test_token.py
+++
@@ -1,7 +0,0 @@
-from srcluslib.predata import srclusdata
-srclusdata = srclusdata()
-srclusdata.createPoolGetFromUrl(4)
-# srclusdata.runGetFromUrl([0, 100])
-# all_data, err_data = srclusdata.getWordFromUrl(36068586)
-# all_data = srclusdata.tokenizeText("Note8 Note 8 Samsung Iphone7 ไอโฟน XS ซัมซุง Fujifilm omd-9 รูปพ่อหมีขากลับจากฟิตเนสค่าา")
-# print(all_data)
