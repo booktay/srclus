8b32e1b330a0c9471fcbf49050337f265c0bdf71
Siwanont Sittinam <siwanont1@gmail.com>
2019-02-05 17:59:17 +0700

    Add tflog

--- core/v1.model.py
+++ core/v1.model.py
@@ -42,44 +42,12 @@ data_index = 0
 
 
 def word2vec_basic(log_dir):
-  """Example of building, training and visualizing a word2vec model."""
-  # Create the directory for TensorBoard variables if there is not.
-  if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-
-  # Step 1: Download the data.
-  url = 'http://mattmahoney.net/dc/'
-
-  # pylint: disable=redefined-outer-name
-  def maybe_download(filename, expected_bytes):
-    """Download a file if not present, and make sure it's the right size."""
-    local_filename = os.path.join('./', filename)
-    if not os.path.exists(local_filename):
-      local_filename, _ = urllib.request.urlretrieve(url + filename,
-                                                     local_filename)
-    statinfo = os.stat(local_filename)
-    if statinfo.st_size == expected_bytes:
-      print('Found and verified', filename)
-    else:
-      print(statinfo.st_size)
-      raise Exception('Failed to verify ' + local_filename +
-                      '. Can you get to it with a browser?')
-    return local_filename
-
-  filename = maybe_download('text8.zip', 31344016)
-
-  # Read the data into a list of strings.
-  def read_data(filename):
-    """Extract the first file enclosed in a zip file as a list of words."""
-    with zipfile.ZipFile(filename) as f:
-      data = tf.compat.as_str(f.read(f.namelist()[0])).split()
-    return data
-
   def getVocabulary(datas):
     words = []
     for word in datas:
-        words += word
-    return set(words)
+      for line in word:
+          words += line.encode("utf-8")
+    return words
 
   foldername = input('[Input] folder name : ')
   datas = []
