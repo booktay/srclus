34e95c74a32fdba356db4895ce15010095b0e630
SIwanont Sittinam <siwanont1@gmail.com>
2019-04-10 00:44:49 +0700

    Init refactor model dir tokenize file

--- core/srcluslib/model/tokenize.py
+++ core/srcluslib/model/tokenize.py
@@ -3,16 +3,97 @@
 # author: Siwanont Sittinam
 # description: model/tokenize module
 
+# General Module
+import os
+import sys
+import re
+
 # Import NLP Module
-# import deepcut
 from pythainlp.tag import pos_tag as thai_tag
+from nltk import pos_tag as eng_tag
 from pythainlp.util import is_thai
 from pythainlp.tokenize import dict_word_tokenize
-from nltk import pos_tag as eng_tag
-# My Module
-from ..corpus.customwords import customwords
+# import deepcut
 
-class tokenize:
+# My Module
+sys.path.insert(0, os.path.abspath('..'))
+from corpus.stopwords import Stopwords
+from corpus.customwords import Customwords
+from utility.iorq import IORQ
+
+'''
+------------- Statuscode -------------
+-- 5XX Series
+500 : OK
+501 : Replaceurl Error
+502 : Filtertheng Error
+503 : Removestopword Error
+'''
+
+
+class Preprocess:
+    # Init
+    def __init__(self):
+        self.stopwords = Stopwords()
+        self.custom_words = Customwords()
+
+    '''                                                                                                                  
+    Replace URL with 2 space                                                                                             
+    ---------------- Input ---------------                                                                               
+    data = "https://www.google.co.th"                                                                                    
+    --------------- Output ---------------                                                                               
+    str(data), int(statuscode)                                                                                           
+    '''
+
+    @staticmethod
+    def replaceurl(data=""):
+        if data != "":
+            text = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', r' ', data)
+            return text, 500
+        else:
+            print("[Error] No Operation in replaceurl function")
+            return "", 501
+
+    '''                                                                                                                  
+    Filter words                                                                                                         
+    ---------------- Input ---------------                                                                               
+    data = ""                                                                                                            
+    --------------- Output ---------------                                                                               
+    str(data), int(statuscode)                                                                                           
+    '''
+
+    @staticmethod
+    def filtertheng(data=""):
+        if data != "":
+            text = re.sub(r'[^a-zA-Z0-9ก-ฮะ-ูเ-์๐-๙\s#@._%)\-]+', r' ', data)
+            text = re.sub(r'([#@._%)\-\s])\1+', r'\1', text)
+            text = re.sub(r'([#@._\-])(\s)', r'\1', text)
+            # TEXT = re.sub(r'(\d+)(\s)([%])', r'\2', TEXT)
+            text = re.sub(r'\s(\d+[\.)]?\d*)\s', r' ', text)
+            # TEXT = re.sub(r'([!-/:-@[-`{-~])\1{1,}', r'\1', data)
+            # TEXT = re.sub(r'(\s)\1{1,}', r' ', TEXT)
+            text = re.sub(r'([a-zA-Zก-ฮ])\1{3,}', r'\1\1', text)
+            text = text.lower()
+            return text, 500
+        else:
+            print("[Error] No operation in filtertheng function")
+            return "", 502
+
+    def removestopword(self, data=[]):
+        if data == "":
+            print("[Error] No Operation in removestopword function")
+            return "", 503
+        stopwordthai, statusth = self.stopwords.languages("thai")
+        stopwordeng, statuseng = self.stopwords.languages("eng")
+        customstopword, statuscustom = self.customwords.target(customtype="stopwords")
+        text = [word for word in data if word not in stopwordeng]
+        text = [word for word in text if word not in stopwordthai]
+        text = [word for word in text if word not in customstopword]
+        text = [word for word in text if len(word) > 1]
+        return text, 500
+
+
+class Tokenize:
     def __init__(self, data=[]):
         self.DATA = data
         self.customwords = customwords()
@@ -51,4 +132,4 @@ class tokenize:
 
 
 if __name__ == "__main__":
-    tokenize(data=['Apple สวัสดี now has Mr.']).run()
+    Tokenize(data=['Apple สวัสดี now has Mr.']).run()
