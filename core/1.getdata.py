ca6b4e705bb9a514d29a7b3430c73c6358eb7613
SIwanont Sittinam <siwanont1@gmail.com>
2019-04-13 00:58:07 +0700

    Prepare for getdata

--- core/1.getdata.py
+++ core/1.getdata.py
@@ -8,54 +8,52 @@ import os
 import sys
 
 # My Module
-# sys.path.insert(0, os.path.abspath(''))
 from srcluslib.utility.iorq import IORQ
 from srcluslib.corpus.pantip import Pantip
-from srcluslib.model.tokenize import Tokenize
+iorq = IORQ()
+pantip = Pantip()
 
-def collectPantip(io=None):
+def collectPantip():
         datas = [{},{}]
-        rangethread = [8000001, 8758650] # Update 12/04/2562 23:50 : 38758650
-        for i in range(rangethread[0], rangethread[1] + 1):
-            thread = str(3*10**7 + i)
+        startthread = 38580000
+        stopthread = 38758650 # Update 12/04/2562 23:50
+        savepath = os.path.join(os.path.abspath("."), "datas", "raw", "39")
+        for i in range(startthread, stopthread + 1):
             try:
-                data, status = io.requestPantipthread(thread=thread, security=True)
+                data, status = pantip.requestthread(thread=str(i))
                 if status == 400 : 
                     datas[0] = {**datas[0], **data}
                 else:
                     datas[0][thread] = ""
                     datas[1][thread] = status
-                if i % 10000 == 0 or i == int(rangethread[1]):
-                    io.writeJson(filename=thread+".json", filepath="datas/raw/39/", data=datas)
+                if i % 10000 == 0 or i == stopthread:
+                    iorq.writejson(filepath=savepath, filename=str(i)+".json", data=datas)
                     datas = [{},{}]
+                    break
             except KeyboardInterrupt:
                 print("[Cancel] Ctrl-c Detection")
-                break
                 sys.exit(0)
 
-def checkCollectPantip(io=None):
-    folderpath="../../datas"
+def checkCollectPantip():
+    folderpath = os.path.join(os.path.abspath("."), "datas", "raw")
     folders = os.listdir(folderpath)
     tempforrequest = []
     count = 0
     for folder in folders:
         filepaths = os.path.join(folderpath, folder)
         for filename in os.listdir(filepaths):
-            data, status = io.readJson(filename=filename, filepath=filepaths)
+            data, status = iorq.readjson(filepath=filepaths, filename=filename)
             count+=1
             for name, status in data[1].items():
                 # io.print([name, status])
                 if status == 401:
                     tempforrequest.append(name)
-    io.print([count, tempforrequest])
+    iorq.print([count, tempforrequest])
 
 
-iorq = IORQ()
-pantip = Pantip()
-data = pantip.requestthread(thread="37681939")  # "35421656", "33869381"
-data = list(data[0].values())[0]
-tk = Tokenize()
-rep, status_url = tk.replaceurl(data=data)
-rep, status_f = tk.filtertheng(data=rep)
-rep, status_nt = tk.numth(data=rep)
-rep, status_t = tk.run(data=rep)
+collectPantip()
+# tk = Tokenize()
+# rep, status_url = tk.replaceurl(data=data)
+# rep, status_f = tk.filtertheng(data=rep)
+# rep, status_nt = tk.numth(data=rep)
+# rep, status_t = tk.run(data=rep)
