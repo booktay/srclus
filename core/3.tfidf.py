d842d7a76423b5370916413dda54058bbc66599a
SIwanont Sittinam <siwanont1@gmail.com>
2019-04-20 04:58:33 +0700

    add full tfidf

--- core/3.tfidf.py
+++ core/3.tfidf.py
@@ -41,27 +41,27 @@ class procfromfile:
         word_all = self.getdata()
         wordsize = str(len(word_all))
         print("[Total] Read " + wordsize + " threads")
-        # #
-        # srclustfidf = TFIDF(word_all)
-        # del word_all
-        # #
-        # response = srclustfidf.weightTfIdf()
-        # print("[Complete] Get Weight")
-        # feature_names = srclustfidf.getFeature()
-        # print("[Complete] Get Feature")
-        # rankwords, statusrank = srclustfidf.getRank(response, feature_names)
-        # del response, feature_names
-        # print("[Total] Thread ["+str(len(rankwords)) + " / " + wordsize + "]")
-        # #
-        # rankword, wordlist = [], []
-        # for n in range(0,len(rankwords)):
-        #     wordlist += rankwords[n]
-        #     rankword.append(rankwords[n])
-        #     if (n > 0 and n % 10000 == 0) or n == len(rankwords) - 1:
-        #         filepathtfidf = os.path.join(tfidf_datas_path, self.time)
-        #         iorq.writejson(filename="tfidf."+ str(n) + ".json", filepath=filepathtfidf, data=rankword)
-        #         rankword = []
-        # iorq.writejson(filename="tfidf.word.json", filepath=filepathtfidf, data=wordlist)
+        #
+        srclustfidf = TFIDF(word_all)
+        del word_all
+        #
+        response = srclustfidf.weightTfIdf()
+        print("[Complete] Get Weight")
+        feature_names = srclustfidf.getFeature()
+        print("[Complete] Get Feature")
+        rankwords, statusrank = srclustfidf.getRank(response, feature_names)
+        del response, feature_names
+        print("[Total] Thread ["+str(len(rankwords)) + " / " + wordsize + "]")
+        #
+        rankword, wordlist = [], []
+        for n in range(0,len(rankwords)):
+            wordlist += rankwords[n]
+            rankword.append(rankwords[n])
+            if (n > 0 and n % 10000 == 0) or n == len(rankwords) - 1:
+                filepathtfidf = os.path.join(tfidf_datas_path, self.time)
+                iorq.writejson(filename="tfidf."+ str(n) + ".json", filepath=filepathtfidf, data=rankword)
+                rankword = []
+        iorq.writejson(filename="tfidf.word.json", filepath=filepathtfidf, data=wordlist)
 
     def convert(self):
         word_all = []
